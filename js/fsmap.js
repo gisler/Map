610728f4def3696d46dc692d6d7f3814ae5bd0f9
gisler <gisler@users.noreply.github.com>
2024-01-14 22:22:09 +0100

    first commit

---
+++ js/fsmap.js
@@ -0,0 +1,254 @@
+/*
+ * TileLayer for Bing Maps.
+ */
+L.TileLayer.QuadKeyTileLayer = L.TileLayer.extend({
+    getTileUrl: function (tilePoint) {
+        return L.Util.template(this._url, {
+            s: this._getSubdomain(tilePoint),
+            q: this._quadKey(tilePoint.x, tilePoint.y, this._getZoomForUrl())
+        });
+    },
+    _quadKey: function (x, y, z) {
+        var quadKey = [];
+        for (var i = z; i > 0; i--) {
+            var digit = '0';
+            var mask = 1 << (i - 1);
+            if ((x & mask) != 0) {
+                digit++;
+            }
+            if ((y & mask) != 0) {
+                digit++;
+                digit++;
+            }
+            quadKey.push(digit);
+        }
+        return quadKey.join('');
+    }
+});
+
+/*
+ * Layer definitions.
+ */
+var BasemapAT_basemap = L.tileLayer(
+    'https://mapsneu.wien.gv.at/basemap/geolandbasemap/{type}/google3857/{z}/{y}/{x}.{format}',
+    {
+        maxZoom: 20,
+        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>',
+        type: 'normal',
+        format: 'png',
+        bounds: [[46.35877, 8.782379], [49.037872, 17.189532]]
+    }
+);
+
+var BasemapAT_highdpi = L.tileLayer(
+    'https://mapsneu.wien.gv.at/basemap/bmaphidpi/{type}/google3857/{z}/{y}/{x}.{format}',
+    {
+        maxZoom: 19,
+        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>',
+        type: 'normal',
+        format: 'jpeg',
+        bounds: [[46.35877, 8.782379], [49.037872, 17.189532]]
+    }
+);
+
+var BasemapAT_orthofoto = L.tileLayer(
+    'https://mapsneu.wien.gv.at/basemap/bmaporthofoto30cm/{type}/google3857/{z}/{y}/{x}.{format}',
+    {
+        maxZoom: 20,
+        attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>',
+        type: 'normal',
+        format: 'jpeg',
+        bounds: [[46.35877, 8.782379], [49.037872, 17.189532]]
+    }
+);
+
+var osmMapnik = new L.TileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        minZoom: 3,
+        maxZoom: 20,
+        maxNativeZoom: 19,
+        attribution: 'Map data © OpenStreetMap contributors'
+    }
+);
+
+var osmOpenTopoMap = new L.TileLayer(
+    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+    {
+        minZoom: 3,
+        maxZoom: 20,
+        maxNativeZoom: 17,
+        attribution: 'Kartendaten: &copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende, <a href="http://viewfinderpanoramas.org">SRTM</a> | Kartendarstellung: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    }
+);
+
+var googleMaps = new L.TileLayer(
+    'https://mt.google.com/vt?&x={x}&y={y}&z={z}',
+    {
+        attribution: "<a href=\'http://maps.google.com/\'>Google</a> Maps",
+        subdomains: "1234",
+        tileSize: 256,
+        minZoom: 3,
+        maxZoom: 20,
+        maxNativeZoom: 20
+    }
+);
+
+var googleSatellite = new L.TileLayer(
+    'https://mt.google.com/vt?lyrs=s&x={x}&y={y}&z={z}',
+    {
+        attribution: "<a href=\'http://maps.google.com/\'>Google</a> Maps Satellite",
+        subdomains: "1234",
+        tileSize: 256,
+        minZoom: 3,
+        maxZoom: 20,
+        maxNativeZoom: 20
+    }
+);
+
+var googleHybrid = new L.TileLayer(
+    'https://mt.google.com/vt?lyrs=y&x={x}&y={y}&z={z}',
+    {
+        attribution: "<a href=\'http://maps.google.com/\'>Google</a> Maps Satellite",
+        subdomains: "1234",
+        tileSize: 256,
+        minZoom: 3,
+        maxZoom: 20,
+        maxNativeZoom: 20
+    }
+);
+
+var bingMaps = new L.TileLayer.QuadKeyTileLayer(
+    'https://ecn.t{s}.tiles.virtualearth.net/tiles/r{q}?g=864&mkt=en-gb&lbl=l1&stl=h&shading=hill&n=z',
+    {
+        subdomains: "0123",
+        minZoom: 3,
+        maxZoom: 20,
+        maxNativeZoom: 19,
+        attribution: "<a href=\'http://maps.bing.com/\'>Bing</a> map data copyright Microsoft and its suppliers"
+    }
+);
+
+var bingAerial = new L.TileLayer.QuadKeyTileLayer(
+    'https://ecn.t{s}.tiles.virtualearth.net/tiles/a{q}?g=737&n=z',
+    {
+        subdomains: "0123",
+        minZoom: 3,
+        maxZoom: 20,
+        maxNativeZoom: 19,
+        attribution: "<a href=\'http://maps.bing.com/\'>Bing</a> map data copyright Microsoft and its suppliers"
+    }
+);
+
+var baseLayers = {
+    "Basemap (STANDARD)": BasemapAT_basemap,
+    "Basemap (HIDPI)": BasemapAT_highdpi,
+    "Basemap (Orthofoto)": BasemapAT_orthofoto,
+    "OpenStreetMap": osmMapnik,
+    "OpenTopoMap": osmOpenTopoMap,
+    "Google Maps": googleMaps,
+    "Google Maps Satellite": googleSatellite,
+    "Google Maps Hybrid": googleHybrid,
+    "Bing Maps": bingMaps,
+    "Bing Aerial View": bingAerial
+};
+
+/*
+ * Set path to default marker images.
+ */
+L.Icon.Default.imagePath = 'images/';
+
+/*
+ * Initialization of map.
+ */
+let map = L.map('map', {
+    layers: [osmMapnik],
+    maxBounds: [[90,-500], [-90,500]],
+    worldCopyJump: true,
+    fullscreenControl: true
+});
+
+/*
+ * Remove Ukrainian flag from attribution to keep this politically neutral.
+ */
+map.attributionControl.setPrefix('<a href="https://leafletjs.com" title="A JavaScript library for interactive maps">Leaflet</a>');
+
+/*
+ * Restore last position or fallback to world.
+ */
+if (!map.restoreView()) {
+    map.fitBounds([[75,-160], [-45,160]]);
+}
+
+/*
+ * History control that allows the user to go back in movement history.
+ */
+new L.HistoryControl({
+    maxMovesToSave: 100
+}).addTo(map);
+
+/*
+ * Layer selector with base and overlay layers.
+ */
+let layercontrol = L.control.layers(baseLayers).addTo(map);
+
+/*
+ * A nice scale in the bottom left corner.
+ */
+L.control.scale().addTo(map);
+
+/*
+ * Locate control.
+ */
+L.control.locate({
+    icon: 'fa fa-location-arrow',
+    showPopup: false
+}).addTo(map);
+
+/*
+ * Measure control with km as unit.
+ */
+new L.Control.Measure({
+    position: 'topleft',
+}).addTo(map);
+
+/*
+ * Initialize and configure geocoder.
+ */
+new L.Control.Geocoder({
+        position: 'topleft',
+        defaultMarkGeocode: false,
+    })
+    .on('markgeocode', function(e) {
+        let result = e.geocode || result;
+
+        map.fitBounds(result.bbox);
+
+        let marker = createMarker(result.center, { title: result.name })
+            .bindPopup(result.html || result.name)
+            .addTo(this._map)
+            .openPopup();
+    })
+    .addTo(map);
+
+/*
+ * Create universal marker with contextmenu.
+ */
+function createMarker(position, options = {}) {
+    let marker = new L.Marker(position, {
+        title: options.title,
+
+        contextmenu: true,
+        contextmenuInheritItems: false,
+        contextmenuItems: [
+            {
+                text: 'Remove marker',
+                callback: function (e) {
+                    map.removeLayer(marker);
+                }
+            }
+        ]
+    });
+
+    return marker;
+}
